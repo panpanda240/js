f85288776ddf3ccefe4437f3b50142ba6924c1e7
pan240 <781828897@qq.com>
2017-05-03 16:01:34 +0800

    点击任意位置出现图片

---
+++ 点击任意位置出现图片/script.js
@@ -0,0 +1,33 @@
+function addLoadEvent(func){
+	var oldonload=window.onload;
+	if(typeof window.onload !='function'){
+		window.onload =func;
+	}else{
+		window.onload=function(){
+			oldonload();
+			func();
+		}
+	}
+}
+addLoadEvent(button);
+
+
+
+function showPic(){
+	var placeholder = document.createElement("img");
+	placeholder.setAttribute("src","image/333.jpg");
+	
+	var show = document.getElementById("show")
+	show.appendChild(placeholder);
+}
+
+
+
+
+
+function button() {
+		document.onclick = function() {
+		return showPic();
+	}
+
+}
